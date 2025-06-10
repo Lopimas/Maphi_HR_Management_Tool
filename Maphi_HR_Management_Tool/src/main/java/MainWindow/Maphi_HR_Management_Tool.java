4d062cb68067b92a503ebbb8980309d70cf8d1d3
Phillip Krahn <phillip.krahn1107@gmail.com>
2025-06-10 13:01:27 +0200

    UI Elemente Hinzugefügt: +Name +Nachname +Geburtsdatum +Abteilung +Logo

--- Maphi_HR_Management_Tool/src/main/java/MainWindow/Maphi_HR_Management_Tool.java
+++ Maphi_HR_Management_Tool/src/main/java/MainWindow/Maphi_HR_Management_Tool.java
@@ -1,4 +1,29 @@
 package MainWindow;
 
-public class Maphi_HR_Management_Tool {
+import javax.swing.*;
+
+public class Maphi_HR_Management_Tool extends JFrame {
+    private JPanel myPanel;
+    private JTextField NamenTextFeld;
+    private JTextField textField1;
+    private JRadioButton männlichRadioButton;
+    private JRadioButton weiblichRadioButton;
+    private JRadioButton diversRadioButton;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+
+    public Maphi_HR_Management_Tool(){
+        setTitle("HR Management Tool");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1920,1080);
+        setContentPane(myPanel);
+        setVisible(true);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+
+    public static void main(String[] args) {
+        new Maphi_HR_Management_Tool();
+    }
 }
