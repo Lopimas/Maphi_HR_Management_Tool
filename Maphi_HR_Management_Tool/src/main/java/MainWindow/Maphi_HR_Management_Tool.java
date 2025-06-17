03eae3588b52717fa174655c530ef211602c6f33
Lopimas <phillip.krahn1107@gmail.com>
2025-06-17 12:27:54 +0200

    -UI angepasst -Radio buttons logik für "Geschlechterauswahl" implementiert -Methode initObjekte() implementiert

--- Maphi_HR_Management_Tool/src/main/java/MainWindow/Maphi_HR_Management_Tool.java
+++ Maphi_HR_Management_Tool/src/main/java/MainWindow/Maphi_HR_Management_Tool.java
@@ -1,19 +1,25 @@
 package MainWindow;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Maphi_HR_Management_Tool extends JFrame {
     private JPanel myPanel;
     private JTextField Name_TextField;
-    private JTextField Nachname_textField;
     private JRadioButton männlichRadioButton;
     private JRadioButton weiblichRadioButton;
     private JRadioButton diversRadioButton;
-    private JComboBox comboBox1;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JList mitarbeiterListe;
+    private JButton speicherButton;
+    private JList list1;
+    private JPanel listPanel;
+    private JPanel geschlechtPanel;
 
     public Maphi_HR_Management_Tool(){     //Konstruktor des Fensters
         setTitle("HR Management Tool");
@@ -22,6 +28,37 @@ public class Maphi_HR_Management_Tool extends JFrame {
         setContentPane(myPanel);
         setVisible(true);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        männlichRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                weiblichRadioButton.setSelected(false);
+                diversRadioButton.setSelected(false);
+            }
+        });
+        weiblichRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                männlichRadioButton.setSelected(false);
+                diversRadioButton.setSelected(false);
+            }
+        });
+        diversRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                männlichRadioButton.setSelected(false);
+                weiblichRadioButton.setSelected(false);
+            }
+        });
+    }
+    public void initObjekte(){
+        List<Mitarbeiter> mitarbeiterDaten = new ArrayList();
+        mitarbeiterDaten.add(new Mitarbeiter(new int[]{11,7,2001}, "IT", false, "männlich", 13.0, new String[]{"Phillip","Krahn"}));
+        mitarbeiterDaten.add(new Mitarbeiter(new int[]{20,6,2003}, "F", false, "männlich", 15.0, new String[]{"Marvin","Beck"}));
+        mitarbeiterDaten.add(new Mitarbeiter(new int[]{4,10,2004}, "HR", false, "weiblich", 13.0, new String[]{"Janine","Baum"}));
+        //for (int i = 0; i < mitarbeiterDaten.size(); i++) {
+        //    mitarbeiterListe.add(mitarbeiterDaten.get(i).getMitarbeiter());
+//
+        //}
     }
 
     public static void main(String[] args) {
--- Maphi_HR_Management_Tool/src/main/java/MainWindow/Mitarbeiter.java
+++ Maphi_HR_Management_Tool/src/main/java/MainWindow/Mitarbeiter.java
@@ -3,21 +3,26 @@ package MainWindow;
 
 import java.util.Calendar;
 
-public class Mitarbeiter { //Konstruktor der Mitarbeiter Klasse
+public class Mitarbeiter {
+
     public int[] geburtsdatum = new int[3];
     private int alter;
     public String abteilung;
     public boolean verheitatet;
     public String geschlecht;
     public double stundenlohn;
-
+    String [] name = new String [2];
     public void setAlter(int alterSet){//setter vom alter
         alter = alterSet;
     }
     public int getAlter() {return alter;}//getter vom alter
-    String [] name = new String [2];
+
     //String vorname = ;
     //name[0] =
+
+    //public String getMitarbeiter(){
+    //    return (toString(alter) + abteilung);
+    //}
 
     public int berechneAlter(){
         Calendar c = Calendar.getInstance(); //legt ein Calender Objekt an
@@ -25,7 +30,7 @@ public class Mitarbeiter { //Konstruktor der Mitarbeiter Klasse
         if (c.get(Calendar.DAY_OF_MONTH) > geburtsdatum[0] && c.get(Calendar.MONTH) > geburtsdatum[1]) bAlter = bAlter - 1; //zieht 1 Jahr ab bei nicht vergangenem Geburtstag
         return bAlter; //gibt das alter zurück
     }
-    public Mitarbeiter(int geburtsdatum[], String abteilung, boolean verheitatet, String geschlecht, double stundenlohn, String[] name) {
+    public Mitarbeiter(int geburtsdatum[], String abteilung, boolean verheitatet, String geschlecht, double stundenlohn, String[] name) { //Konstruktor der Mitarbeiter Klasse
 
         this.geburtsdatum= geburtsdatum;
         setAlter(berechneAlter());
